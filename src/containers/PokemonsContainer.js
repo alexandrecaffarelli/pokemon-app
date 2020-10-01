47c43d243fe3a87ed627a88850f66278b3ad413e
Alexandre Caffarelli <58951646+alexandrecaffarelli@users.noreply.github.com>
2020-10-01 18:01:35 +0200

    Feat: complete PokemonsContainer

--- src/containers/PokemonsContainer.js
+++ src/containers/PokemonsContainer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import Pokemon from "../components/Pokemon";
+import { GET_POKEMONS } from "../../graphql/getPokemons";
 
 export default function PokemonsContainer() {
+
+    const { data: { pokemons = [] } = {} } = useQuery(GET_POKEMONS, {
+        variables: { first: 9 },
+    })
+
     return (
-        <p> I am a pokemon container </p>
-    )
+        <div className="container">
+            {pokemons && pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+        </div>
+    );
 } 
