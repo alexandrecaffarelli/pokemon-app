110bd7ee3bd58537029ca0fddcf2d4fa308bfe00
Alexandre Caffarelli <58951646+alexandrecaffarelli@users.noreply.github.com>
2020-10-01 17:12:58 +0200

    Feat: create PokemonsContainer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import ApolloClient from "apollo-boost";
+import  { ApolloProvider } from "@apollo/react-hooks";
+import  PokemonsContainer from "./containers/PokemonsContainer";
 
 export default function App() {
+    const client = new ApolloClient({
+        uri: 'https://graphql-pokemon.now.sh'
+    });
+
     return (
-        <p> I am a Pokemon </p>
+        <ApolloProvider client={client}>
+            <main>
+                <PokemonsContainer />
+            </main>
+        </ApolloProvider>
     )
 }
